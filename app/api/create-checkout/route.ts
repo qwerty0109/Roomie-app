81def3598a04492b5144011a8d35528fa2286e0d
qwerty0109 <joelplavioleta@gmail.com>
2026-04-30 11:20:13 +0200

    Fix version API Stripe

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2026-04-22.basil',
 })
 
 export async function POST(request: Request) {
